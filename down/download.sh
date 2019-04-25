0533f3b6bb0aaa1134b65a5691463a47116c2bd7
shellgithub <shellgithub@users.noreply.github.com>
2019-04-25 17:52:59 +0800

    Update download.sh
    [update] 修改单引号为双引号，解决不能获取变量的问题

--- down/download.sh
+++ down/download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# This script describes where to download the official released binaries needed 
-# It's suggested to download the entire *.tar.gz at https://pan.baidu.com/s/1c4RFaA
+# This script describes where to download the official released binaries needed
+# It"s suggested to download the entire *.tar.gz at https://pan.baidu.com/s/1c4RFaA
 
 # example releases
-K8S_VER=v1.10.4
+K8S_VER=v1.13.4
 ETCD_VER=v3.3.8
-DOCKER_VER=17.03.2-ce
-CNI_VER=v0.6.0
+DOCKER_VER=18.09.2
+CNI_VER=v0.7.5
 DOCKER_COMPOSE=1.18.0
-HARBOR=v1.5.2
+HARBOR=v1.5.4
 
 echo "\nNote1: Before this script, please finish downloading binaries manually from following urls."
 echo "\nNote2：If binaries are not ready, use `Ctrl + C` to stop this script."
@@ -32,38 +32,38 @@ echo "\n----download docker-compose at:"
 echo https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE}/docker-compose-Linux-x86_64
 
 echo "\n----download harbor-offline-installer at:"
-echo https://github.com/vmware/harbor/releases/download/${HARBOR}/harbor-offline-installer-${HARBOR}.tgz
+echo https://storage.googleapis.com/harbor-releases/harbor-offline-installer-${HARBOR}.tgz
 
 echo "\n----download cni plugins at:"
 echo https://github.com/containernetworking/plugins/releases
 
 sleep 30
 
-### prepare 'cfssl' cert tool suit
+### prepare "cfssl" cert tool suit
 echo "\nMoving 'cfssl' to 'bin' dir..."
 if [ -f "cfssl_linux-amd64" ]; then
   mv -f cfssl_linux-amd64 ../bin/cfssl
 else
-  echo Please download 'cfssl' at 'https://pkg.cfssl.org/R1.2/cfssl_linux-amd64'
+  echo Please download 'cfssl' at "https://pkg.cfssl.org/R1.2/cfssl_linux-amd64"
 fi
 if [ -f "cfssljson_linux-amd64" ]; then
   mv -f cfssljson_linux-amd64 ../bin/cfssljson
 else
-  echo Please download 'cfssljson' at 'https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64'
+  echo Please download "cfssljson" at "https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64"
 fi
 if [ -f "cfssl-certinfo_linux-amd64" ]; then
   mv -f cfssl-certinfo_linux-amd64 ../bin/cfssl-certinfo
 else
-  echo Please download 'cfssl-certinfo' at 'https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64'
+  echo Please download "cfssl-certinfo" at "https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64"
 fi
 
-### prepare 'etcd' binaries
+### prepare "etcd" binaries
 if [ -f "etcd-${ETCD_VER}-linux-amd64.tar.gz" ]; then
   echo "\nextracting etcd binaries..."
   tar zxf etcd-${ETCD_VER}-linux-amd64.tar.gz
   mv -f etcd-${ETCD_VER}-linux-amd64/etcd* ../bin
 else
-  echo Please download 'etcd-${ETCD_VER}-linux-amd64.tar.gz' first
+  echo Please download "etcd-${ETCD_VER}-linux-amd64.tar.gz" first
 fi
 
 ### prepare kubernetes binaries
@@ -77,7 +77,7 @@ if [ -f "kubernetes-server-linux-amd64.tar.gz" ]; then
   mv -f kubernetes/server/bin/kube-proxy ../bin
   mv -f kubernetes/server/bin/kube-scheduler ../bin
 else
-  echo Please download 'kubernetes-server-linux-amd64.tar.gz' first
+  echo Please download "kubernetes-server-linux-amd64.tar.gz" first
 fi
 
 ### prepare docker binaries
@@ -89,18 +89,18 @@ if [ -f "docker-${DOCKER_VER}.tgz" ]; then
     mv -f docker/completion/bash/docker ../roles/docker/files/docker
   fi
 else
-  echo Please download 'docker-${DOCKER_VER}.tgz' first 
+  echo Please download "docker-${DOCKER_VER}.tgz" first
 fi
 
 ### prepare cni plugins, needed by flannel;
-if [ -f "cni-${CNI_VER}.tgz" ]; then
+if [ -f "cni-plugins-amd64-${CNI_VER}.tgz" ]; then
   echo "\nextracting cni plugins binaries..."
-  tar zxf cni-${CNI_VER}.tgz
+  tar zxf cni-plugins-amd64-${CNI_VER}.tgz
   mv -f bridge ../bin
   mv -f flannel ../bin
   mv -f host-local ../bin
   mv -f loopback ../bin
   mv -f portmap ../bin
 else
-  echo Please download 'cni-${CNI_VER}.tgz' first 
+  echo Please download "cni-plugins-amd64-${CNI_VER}.tgz" first
 fi
